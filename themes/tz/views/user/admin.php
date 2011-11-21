acc0533910c4a185ce0376f56ac056e4a1858677
Matvey Vasenin <vaseninm@gmail.com>
2011-11-21 13:35:22 +0400

    Тест ветки

--- themes/tz/views/user/admin.php
+++ themes/tz/views/user/admin.php
@@ -23,7 +23,7 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Laws</h1>
+<h1>Управление пользователями</h1>
 
 <p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
